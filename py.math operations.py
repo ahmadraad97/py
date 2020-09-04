f3a1365d6aecc2ec32abec88bfe88c33d12cf217
ahmadraad97 <69446442+ahmadraad97@users.noreply.github.com>
2020-09-04 08:28:42 +0300

    Add files via upload

---
+++ py.math operations.py
@@ -0,0 +1,59 @@
+a=10
+b=5
+d=-7
+h=8.99
+
+print(a+b)
+# 15
+print(a-b)
+# 5
+print(a*b)
+# 50
+print(a/b)
+# 2.0
+
+# باقي القسمة
+print(a%b)
+# 0
+
+# القسمة مع رقم صحيح
+print(a//b)
+# 2
+
+# الأس
+print(a**b)
+# 100000
+
+# هل يساوي
+print(a==b)
+# False
+
+print(a>b)
+# True
+print(a>=b)
+# True
+print(a<b)
+# False
+print(a<=b)
+# False
+print(b<a<20)
+# True
+
+# قيمة مطلقة
+print(abs(d))
+# 7
+
+# تحويل الى رقم صحيح بدون تقريب لحد 15 رقم بعد الفاصلة
+print(int(h))
+# 8
+
+# رقمين حاصل القسمة والباقي
+print(divmod(a,b))
+# (2, 0)
+
+# الأس
+print(pow(a,b))
+# 100000
+
+
+
