eb90a94825f125af2b77639293f6281e6ccc8eb5
ahmadraad97 <69446442+ahmadraad97@users.noreply.github.com>
2020-09-04 07:55:06 +0300

    Add files via upload

---
+++ py.variables.py
@@ -0,0 +1,53 @@
+# المتغيرات
+A,a,n,m,b="ahmad",5.6,12,True,9j
+# يمكن كتابة اكثر من نوع وحالة الحرف مؤثرة
+
+# انواع المتغيرات
+int,str,float,bool,list,set,tuple,dict,complex
+
+# التحويل 
+x=7
+y=float(x)
+print(y)
+# 7.0
+s=9
+l=str(s)
+print(l)
+# 9
+k=complex(s,y)
+print(k)
+# (9+7j)
+# طباعة الارقام العقدية
+print(k.real)
+# 9.0
+print(k.imag)
+# 7.0
+
+# القيم المنطقية
+print(bool(5.2))
+# True
+print(bool(0))
+# False
+print(bool('ahmad'))
+# True
+print(bool())
+# False
+
+# المتغير الثنائي
+print(bin(50))
+# 0b110010
+print(type(bin(50)))
+# <class 'str'>
+
+
+
+
+
+
+
+
+
+
+
+
+
