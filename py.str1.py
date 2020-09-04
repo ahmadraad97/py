db589777fa455ff16040a0d90a0a9bc1c9fd84d8
ahmadraad97 <69446442+ahmadraad97@users.noreply.github.com>
2020-09-04 14:47:40 +0300

    Add files via upload

---
+++ py.str1.py
@@ -0,0 +1,30 @@
+a ='hello python'
+b="hello python"
+c=a[0]
+print(c)
+# h
+print(a[2],a[-1],a[-8])
+# l n o
+
+print(a*3)
+# hello pythonhello pythonhello python
+
+print(a[2:12])
+# llo python
+
+print(a[:4])
+# hell
+
+print(a[2:])
+# llo python
+
+print(a[:])
+# hello python
+
+
+
+
+
+
+
+
